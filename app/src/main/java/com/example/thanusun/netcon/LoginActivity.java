2d616ab553818af7bd048c171149859c72e64b56
coolmanbat2 <thanusun.thayaparan@mail.utoronto.ca>
2017-12-02 18:11:37 -0500

    Added functionality to go to the screen with two tabs.

--- app/src/main/java/com/example/thanusun/netcon/LoginActivity.java
+++ app/src/main/java/com/example/thanusun/netcon/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.thanusun.netcon;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -86,6 +87,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 attemptLogin();
             }
         });
@@ -182,11 +184,13 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             // form field with an error.
             focusView.requestFocus();
         } else {
-            // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
+            // Show a progress spinner, and
+            // enter the second window with the options to do more things.
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
-            mAuthTask.execute((Void) null);
+            Intent secWin = new Intent(this, sendingInfo.class);
+            mAuthTask.execute();
+            startActivity(secWin);
         }
     }
 
